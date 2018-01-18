dee623ba5cda89f03a6788af14ebdda05ee480ab
goaler <18395587850@163.com>
2018-01-18 20:08:07 +0800

    统一异常捕捉

---
+++ src/main/java/org/corgiking/exception/ExceptionHandler.java
@@ -0,0 +1,20 @@
+package org.corgiking.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Component
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		// TODO Auto-generated method stub
+		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
+
+}
